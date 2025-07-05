e596652f32b5f063499df417680187f559269e4f
Neamix <abdalrhmanhussin44@gmail.com>
2025-07-05 04:52:13 +0300

    revive the code

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,20 +2,20 @@
 
 import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { FaGoogle } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import Link from "next/link";
 import { useValidateForm, ValidationErrors } from "@/lib/validation";
+import { useAuthenticate } from "@/hooks/auth/useAuthenticate";
 
 
 // Components
 import PasswordInput from "./__components/password-input";
 import EmailInput from "./__components/email-input";
-import { useAuthenticate } from "@/hooks/auth/useAuthenticate";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 export default function SignComponent () {
     const {data:session} = authClient.useSession();
@@ -59,13 +59,13 @@ export default function SignComponent () {
                 return false;
             }        
 
-
+            // Login or return fail message
             const authentication = await useAuthenticate({email, password});
-
             if (authentication.status) return router.push('/');
             else setErrors({
                 authentication:[ authentication.message]
             })
+
         } catch (error) {
             setIsLoading(false);
             setErrors({
@@ -80,68 +80,72 @@ export default function SignComponent () {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <Card className="!max-w-[454px] p-6">
+                <Card className="!max-w-[454px] p-6">
 
-                <div className="card-header">
-                    <h2 className="text-headline-auth">Log in to your account</h2>
-                    
-                    <p className="text-subline-auth">Connect to MeetPilot with:</p>
+                    <div className="card-header">
+                        <h2 className="text-headline-auth">Log in to your account</h2>
+                        
+                        <p className="text-subline-auth">Connect to MeetPilot with:</p>
 
-                    <div className="flex justify-between gap-2 mt-3">
+                        <div className="flex justify-between gap-2 mt-3">
 
-                        {/* <Button className="pulse-on-click h-[35px] !w-[165px]">
-                            <FaGoogle className="text-[10px] mr-2"/>
-                            <span className="text-[13px]">Google</span>
-                        </Button>
+                            <Button className="pulse-on-click h-[35px] !w-[165px]" onClick={() => useSocailLogin({provider: 'google'})}>
+                                <FaGoogle className="text-[10px] mr-2"/>
+                                <span className="text-[13px]">Google</span>
+                            </Button>
 
-                        <Button className="pulse-on-click h-[35px] !w-[165px]">
-                            <FaTwitter className="text-[10px] mr-2"/>
-                            <span className="text-[13px]">Twitter</span>
-                        </Button> */}
+                            <Button className="pulse-on-click h-[35px] !w-[165px]" onClick={() => useSocailLogin({provider: 'facebook'})}>
+                                <FaFacebook className="text-[10px] mr-2"/>
+                                <span className="text-[13px]">Facebook</span>
+                            </Button>
+
+                        </div>
 
                     </div>
 
-                </div>
+                    <div className="text-inline flex items-center gap-3">
+                        <hr className="flex-1"/>
+                        <p className="text-[12px] uppercase text-gray-500">Or log in with your Email</p>
+                        <hr className="flex-1"/>
+                    </div>
 
-                <div className="text-inline flex items-center gap-3">
-                    <hr className="flex-1"/>
-                    <p className="text-[12px] uppercase text-gray-500">Or log in with your Email</p>
-                    <hr className="flex-1"/>
-                </div>                
-                <div className="form">
-                    <EmailInput
-                        onChange={(e) => setEmail(e.target.value)}
-                        error={errors.email ? errors.email[0] : ''}
-                    />
-                    <span className="text-error">{errors.authentication}</span>
-                </div>
+                    <form className="w-full">               
+                        <div className="form">
+                            <EmailInput
+                                onChange={(e) => setEmail(e.target.value)}
+                                error={errors.email ? errors.email[0] : ''}
+                            />
+                            <span className="text-error">{errors.authentication}</span>
+                        </div>
 
-                <div className="form relative">
-                    <PasswordInput
-                        onChange={(e) => setPassword(e.target.value)}
-                        error={errors.password ? errors.password[0] : ''}
-                    />
-                    <p className="text-[11px] text-blue-900 mt-2 font-[500]">
-                        <Link href={'/auth/forget-password'}>Forget your password ?</Link>
-                    </p>
-                </div>
+                        <div className="form relative mt-4">
+                            <PasswordInput
+                                onChange={(e) => setPassword(e.target.value)}
+                                error={errors.password ? errors.password[0] : ''}
+                            />
+                            <p className="text-[11px] text-blue-900 mt-2 font-[500]">
+                                <Link href={'/auth/forget-password'}>Forget your password ?</Link>
+                            </p>
+                        </div>
 
 
-                <Button
-                    onClick={() => submitLoginForm()}
-                    disabled={isLoading}
-                >
-                    {isLoading ? (
-                    <>
-                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                        Signing in...
-                    </>
-                    ) : (
-                    "Sign in"
-                    )}
-                </Button>
+                        <Button
+                            className="w-full mt-2"
+                            onClick={() => submitLoginForm()}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (
+                            <>
+                                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                Signing in...
+                            </>
+                            ) : (
+                            "Sign in"
+                            )}
+                        </Button>
+                    </form>
 
-            </Card>
+                </Card>
         </div>
     );  
    
