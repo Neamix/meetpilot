1d0fa45065e5b9d9562679d081e9006af64ce605
Neamix <abdalrhmanhussin44@gmail.com>
2025-07-06 19:50:43 +0300

    Fix sidebar toggle

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function AdminLayout ({ children }: { children: React.ReactNode }
             <AppSidebar />
             <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                <div className="flex items-center gap-2 px-4 rtl:flex-row-reverse flex-1">
+                <div className="flex items-center gap-2 px-4 flex-1">
                 <SidebarTrigger 
                     className="-ml-1 rtl:-mr-1 rtl:ml-0"
                     aria-label={t('toggleSidebar')}
